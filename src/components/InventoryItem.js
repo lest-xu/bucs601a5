c41d30b6e17e92032644454390c62b4770be6229
lest-xu <autodataxx@gmail.com>
2024-06-08 20:54:39 -0400

    update

--- src/components/InventoryItem.js
+++ src/components/InventoryItem.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import './../styles.css';
 
 class InventoryItem extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
+    constructor(props) {
+        super(props);
+        this.state = { product: props.product };
+    }
 
     render() {
         return (
-            <div className="grid-item">Project 1</div>
+            <div className="grid-item" style={{
+               backgroundImage: `url(${this.state.product.imgUrl})`,
+               backgroundSize: '80px',
+               backgroundRepeat: 'no-repeat',
+               backgroundPosition: 'bottom 10px right 10px'
+            }}>
+                <p><b>{this.state.product.name}</b></p>
+                <p>SKU: {this.state.product.sku}</p>
+                <p>Available: {this.state.product.quantity}</p>
+                <p className="price">Price: ${this.state.product.price}</p>
+            </div>
         );
     }
 }
--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import InventoryItem from './InventoryItem';
-const api = '';
+const api = 'https://raw.githubusercontent.com/lest-xu/bucs601a5/main/src/models/inventory.json';
 
 class InventoryList extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+        this.state = { inventories: [] };
+    }
 
+    componentDidMount() {
+        fetch(api)
+            .then(res => res.json())
+            .then(data => this.setState({ inventories: data }))
+            .catch(error => console.log(error));
+    }
 
     render() {
         return (
             <section className="products">
                 <h2>Inventory 🍎</h2>
                 <div className="grid-container">
-                    <InventoryItem />
+                    {this.state.inventories.map(item => (
+                        // console.log(item.name)
+                        <InventoryItem key={item.sku} product={item}></InventoryItem>
+                    ))}
                 </div>
             </section>
         );
